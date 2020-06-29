acc4d75e1d6d8a5ca3dbe5b7a8da46ee67d5616e
mstahl <stahlmatias@gmail.com>
2020-06-29 15:14:08 -0300

    Ways of checking patterns

---
+++ check_pattern.sh
@@ -0,0 +1,8 @@
+for CDTRACK in *
+do
+    if [[ "$CDTRACK" =~ "([[:alpha:][:blank:]]*)- ([[:digit:]]*) - (.*)$" ]]
+    then
+        echo Track ${BASH_REMATCH[2]} is ${BASH_REMATCH[3]}
+        mv "$CDTRACK" "Track${BASH_REMATCH[2]}"
+    fi
+done
