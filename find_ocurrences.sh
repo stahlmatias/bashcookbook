39382e517fdb37b85e5dc66251cb0f161385b772
mstahl <stahlmatias@gmail.com>
2020-06-29 15:21:14 -0300

    Find ocurrences

---
+++ find_ocurrences.sh
@@ -0,0 +1,10 @@
+grep printf *.c
+
+# If you don’t want to see the specific filenames, 
+# you may turn this off using the -h option to grep
+
+# If you don’t want to see the actual lines from the file, 
+# but only a count of the number of times the expression 
+# is found, then use the -c option
+
+
